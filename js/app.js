b0ffd6278200a9d5cbb7219459f69f291f37f21b
VictorIsacc <vigabar@gmail.com>
2026-09-14 02:56:15 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import {
   getWriteWebhookUrl, saveWriteWebhookUrl, clearWriteWebhookUrl, saveContabilidadValues,
   saveIngresoValues, saveAhorroValues
 } from "./cloud-data.js?v=20260914c-tabs";
-import { initLegacyTabs } from "./legacy-tabs-20260914d.js";
+import { initLegacyTabs } from "./legacy-tabs-20260914d.js?v=20260914e-red";
 
 const $=id=>document.getElementById(id);
 const qsa=s=>[...document.querySelectorAll(s)];
